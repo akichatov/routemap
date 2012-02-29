827e6c37068a1420fb732347ae9b4b2215c23322
akichatov <kichatov.a@gmail.com>
2012-02-29 23:20:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Routemap::Application.config.secret_token = '8eccb98d5860668f8ea008c727a8c2cd4ec4fc1987ec03684eeb4eec62c542c2d7f97b187b6602fb450a76315bf1f44265045f5f9af8a71f87cb79e791091768'
