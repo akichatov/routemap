70f88c2cae8d2997a83e89de76301f1684584fac
akichatov <kichatov.a@gmail.com>
2013-01-12 01:50:29 +0200

    use session secret from env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Routemap::Application.config.secret_token = '8eccb98d5860668f8ea008c727a8c2cd4ec4fc1987ec03684eeb4eec62c542c2d7f97b187b6602fb450a76315bf1f44265045f5f9af8a71f87cb79e791091768'
+Routemap::Application.config.secret_token = ENV['RM_SECRET'] || '8eccb98d5860668f8ea008c727a8c2cd4ec4fc1987ec03684eeb4eec62c542c2d7f97b187b6602fb450a76315bf1f44265045f5f9af8a71f87cb79e791091768'
