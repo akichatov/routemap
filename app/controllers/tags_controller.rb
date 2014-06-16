2340c2e12c80a43bf5a47df669f75334f52a193d
akichatov <kichatov.a@gmail.com>
2014-06-16 23:59:15 +0300

    tags multi-view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,9 +7,9 @@ class TagsController < ApplicationController
   def find_tag
     ids = params[:id].split(',')
     if ids.size > 1
-      @tag = Tag.new(name: 'tracks', code: params[:id], tracks: Track.where(code: ids).start_at_ordered)
+      @tag = Tag.new(name: 'tracks', code: params[:id], tracks: Track.joins(:tags).where("tags.code IN (?)", ids).start_at_ordered)
     else
-      @tag = Tag.find_by_code!(params[:id])
+      @tag = Tag.find_by_code!(ids)
     end
   end
 
